aa85124645eb6f06292c4598ae7cdc4b8d3015f1
big-d-35 <44974117+big-d-35@users.noreply.github.com>
2019-08-19 17:31:25 +0300

    Страницы Home agent начало на VUE

---
+++ Components/Agent-Home/VUE-diff-product-home-page.js
@@ -0,0 +1,49 @@
+const homePageFirstBlock = new Vue ({
+    el: '#home-page-first-block',
+    data:{
+        //TITLE
+        firstBlockHeadNameRU1: 'KОНЦЕПЦИИ ОТДЫХА,',
+        firstBlockHeadNameRU2: 'СОЗДАННЫЕ TUI',
+        firstBlockHeadNameBY1: 'KОНЦЕПЦИИ ОТДЫХА,',
+        firstBlockHeadNameBY2: 'СОЗДАННЫЕ TUI',
+        firstBlockHeadNameUA1: 'KОНЦЕПЦИИ ОТДЫХА,',
+        firstBlockHeadNameUA2: 'СОЗДАННЫЕ TUI',
+        firstBlockHeadNameEE1: 'PUHKUSE KONTSEPTSIOONID,',
+        firstBlockHeadNameEE2: 'TUI',
+        firstBlockHeadNameLT1: 'POILSIO KONCEPCIJOS,',
+        firstBlockHeadNameLT2: 'SUKURTOS TUI',
+        firstBlockHeadNameLV1: 'ATPŪTAS KONCEPCIJAS,',
+        firstBlockHeadNameLV2: 'KAS RADĪTAS TUI',
+        
+        //COMMENT
+        firstBlockHeadCommentRU1: 'Мы знаем, какой отдых вам нужен,',
+        firstBlockHeadCommentRU2: 'потому что вы нам сами об этом рассказали!',
+        firstBlockHeadCommentBY1: 'Мы знаем, какой отдых вам нужен,',
+        firstBlockHeadCommentBY2: 'потому что вы нам сами об этом рассказали!',
+        firstBlockHeadCommentUA1: 'Мы знаем, какой отдых вам нужен,',
+        firstBlockHeadCommentUA2: 'потому что вы нам сами об этом рассказали!',
+        firstBlockHeadCommentEE1: 'Teame, millist puhkust te vajate,',
+        firstBlockHeadCommentEE2: 'sest olete ise meile sellest rääkinud!',
+        firstBlockHeadCommentLT1: 'Mes žinome, kokių reikia atostogų,',
+        firstBlockHeadCommentLT2: 'nes Jūs mums apie tai pasakėte!',
+        firstBlockHeadCommentLV1: 'Mēs zinām, kāda atpūta jums ir nepieciešama,',
+        firstBlockHeadCommentLV2: 'jo jūs paši par to mums pastāstījāt!',
+        
+        //ПРЕЗЕНТАЦИЯ
+        firstBlockHeadPresentaionLink: 'https://agent.tui.ru/getmedia/244095d3-62f8-4aff-81ea-162043e96d27/NEW-Concept-Presentation_B2B_11-07',
+        firstBlockHeadPresentaionRU: 'Презентация Дифф продукта',
+        firstBlockHeadPresentaionBY: 'Презентация Дифф продукта',
+        firstBlockHeadPresentaionUA: 'Презентация Дифф продукта',
+        firstBlockHeadPresentaionEE: 'Vali konsept',
+        firstBlockHeadPresentaionLT: 'Prezentacija',
+        firstBlockHeadPresentaionLV: 'Diferencēta produkta prezentācija',
+        
+        //Details
+        firstBlockHeadDeatilsRU: '* Концепции отдыха TUI - это отдых в новом формате, где учтены интересы всех поколений.',
+        firstBlockHeadDeatilsBY: '* Концепции отдыха TUI - это отдых в новом формате, где учтены интересы всех поколений.',
+        firstBlockHeadDeatilsUA: '* Концепции отдыха TUI - это отдых в новом формате, где учтены интересы всех поколений.',
+        firstBlockHeadDeatilsEE: '* TUI puhkuse konseptsioon - uue formaadi puhkus, kus on võetud arvesse igas vanuses puhkajate huvid.',
+        firstBlockHeadDeatilsLT: '* TUI poilsio koncepcijos - tai naujas poilsio formatas, kur atsižvelgiama į visų kartų interesus.',
+        firstBlockHeadDeatilsLV: '* TUI atpūtas koncepcijas - atpūta jaunā formātā, kas ņem vērā visu paaudžu intereses.',
+    }
+})
